0c81958dad8b6594cabbfcdb905f6242e4aca78f
Cynthia Zhang <zhangcyn08@gmail.com>
2020-07-11 17:06:53 -0400

    redirect to new_tweet_path after tweet form submission

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,18 @@ class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
 	end
+
+	def create
+		# @tweet = Tweet.new
+		# @tweet.content = params["tweet"]["content"]
+		# @tweet.save
+
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save
+		redirect_to new_tweet_path
+	end
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
 end
