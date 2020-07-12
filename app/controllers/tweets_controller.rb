95949564f0df9a5add8399527ae7c9549bc5560c
Cynthia Zhang <zhangcyn08@gmail.com>
2020-07-12 12:07:26 -0400

    Added flash messages

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,8 +10,10 @@ class TweetsController < ApplicationController
 
 		@tweet = Tweet.new(tweet_params)
 		@tweet.save
+		flash[:success] = "Your tweet was successfully published!"
 		redirect_to new_tweet_path
 	end
+
 
 	def tweet_params
 		params.require(:tweet).permit(:content)
